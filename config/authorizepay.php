df670c4a49628330c09d0b04e743a86d5e8bf4be
Serhii <sergunik@gmail.com>
2019-09-13 13:09:15 +0000

    Apply fixes from StyleCI

--- config/authorizepay.php
+++ config/authorizepay.php
@@ -1,11 +1,11 @@
 <?php
-if (!defined('AUTHORIZENET_LOG_FILE'))
-{
-    define( 'AUTHORIZENET_LOG_FILE',  env('AUTHORIZENET_LOG_FILE'));
+
+if (!defined('AUTHORIZENET_LOG_FILE')) {
+    define('AUTHORIZENET_LOG_FILE', env('AUTHORIZENET_LOG_FILE'));
 }
 
 return [
-    'MERCHANT_LOGIN_ID' => env('MERCHANT_LOGIN_ID'),
+    'MERCHANT_LOGIN_ID'        => env('MERCHANT_LOGIN_ID'),
     'MERCHANT_TRANSACTION_KEY' => env('MERCHANT_TRANSACTION_KEY'),
-    'test' => 'works!'
+    'test'                     => 'works!',
 ];
--- src/AuthorizeStatuses.php
+++ src/AuthorizeStatuses.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace AuthorizePay;
 
 trait AuthorizeStatuses
